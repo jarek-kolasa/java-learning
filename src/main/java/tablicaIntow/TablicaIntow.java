2b96838860a32e44c8eda6527ecb698e6c46ae2e
Jaroslaw Kolasa <kolasa.jarek@gmail.com>
2019-01-13 17:58:06 +0100

    First commit

---
+++ src/main/java/tablicaIntow/TablicaIntow.java
@@ -0,0 +1,15 @@
+package tablicaIntow;
+
+/**
+ * stworz nowa tablice ze 100 intow i wypisz:
+ * a) co drugi element
+ * b) parzyste elementy
+ * c) liczby podzielne jednocze�nie przez 2 i 3
+ * d) liczby pierwsze
+ * e) sume liczb nieparzystych
+ * f) iloczyn wszystkich liczb podzielnych przez 5
+ */
+
+public class TablicaIntow {
+
+}
---
+++ src/test/java/tablicaIntowTest/TablicaIntowTest.java
@@ -0,0 +1,5 @@
+package tablicaIntowTest;
+
+public class TablicaIntowTest {
+
+}
